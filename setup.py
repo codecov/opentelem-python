2863880ed852e4f6af8ed038131959540631b477
Eli Hooten <eli@codecov.io>
2022-01-12 20:51:10 -0600

    Bump to 0.0.6
    Using 0.0.6 after fixing setup.py errors discovered during packaging.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="codecovopentelem",
-    version="0.0.5",
+    version="0.0.6",
     description="Implements Runtime Insights Support for Python Project",
     long_description=long_description,
     long_description_content_type='text/markdown',
